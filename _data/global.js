7000c70476482adaf2d13500543eefa5eead37d6
Antonio Gatta <a.gatta@xeader.com>
2022-08-05 13:12:27 +0200

    data/global

---
+++ _data/global.js
@@ -0,0 +1,30 @@
+/*
+ * Xeader Studios
+ *
+ * NOTICE OF LICENCE
+ *
+ * This source file is subject to the EULA
+ * that is bundled with this package in the file LICENSE.txt
+ * It is also available through th world-wide-web at this URL:
+ * https://xeader.com/LICENCE-CE.txt
+ *
+ * @category eleventy
+ * @package eleventy
+ *
+ * @author Antonio Gatta <a.gatta@xeader.com>
+ * @url http://xeader.com
+ * @copyright Copyright (c) 2022 Xeader Studios
+ * @license All right reserved
+ */
+
+module.exports = {
+    it: {
+        madeBy: {
+            name: 'Bluelabs',
+            where: 'Italy',
+            with: 'Eleventy',
+            when: '2022',
+            url: 'https://www.bluelabs.it'
+        }
+    },
+};
