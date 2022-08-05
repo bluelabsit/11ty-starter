8e99beb03e9b944a7026af99976c912903d7c579
Antonio Gatta <a.gatta@xeader.com>
2022-08-05 15:59:36 +0200

    BrowserSync config

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
-const fs = require("fs");
 const { markdownLibrary } = require("./.markdown.js");
 
 const eleventyPlugins = require("./config/plugins.js");
 const eleventyFilters = require("./config/filters.js");
+const browserSync = require("./config/browserSync.js");
 
 module.exports = function(eleventyConfig) {
     // Init Plugins
@@ -28,30 +28,21 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("static");
     eleventyConfig.addPassthroughCopy("./src/assets/css/prism.css");
 
-    eleventyConfig.addWatchTarget("./src/**/*.js")
-    eleventyConfig.addWatchTarget("./src/assets/css/**/*.scss")
-    eleventyConfig.addWatchTarget("./src/**/*.md")
-
     eleventyConfig.setLibrary("md", markdownLibrary);
 
-    eleventyConfig.setBrowserSyncConfig({
-        files: '*',
-        ignore: ['_site', '.gitignore', 'node_modules'],
-        callbacks: {
-            ready: function(err, browserSync) {
-                const content_404 = fs.readFileSync('_site/404.html');
+    eleventyConfig.setBrowserSyncConfig(browserSync);
 
-                browserSync.addMiddleware("*", (req, res) => {
-                    // Provides the 404 content without redirect.
-                    res.writeHead(404, {"Content-Type": "text/html; charset=UTF-8"});
-                    res.write(content_404);
-                    res.end();
-                });
-            },
-        },
-        ui: false,
-        ghostMode: false
-    });
+    /**
+     * Watchers
+     */
+    // JS
+    eleventyConfig.addWatchTarget("./src/**/*.js")
+
+    // SCSS
+    eleventyConfig.addWatchTarget("./src/assets/css/**/*.scss")
+
+    // MD
+    eleventyConfig.addWatchTarget("./src/**/*.md")
 
     return {
         templateFormats: [
@@ -59,12 +50,10 @@ module.exports = function(eleventyConfig) {
             "njk",
             "html",
         ],
-
         pathPrefix: "/",
         markdownTemplateEngine: "njk",
         htmlTemplateEngine: "njk",
         dataTemplateEngine: false,
-
         dir: {
             input: "src",
             includes: "_includes",
