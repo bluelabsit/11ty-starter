471bb481704515034dc890244432b01737aa36af
Antonio Gatta <a.gatta@xeader.com>
2022-08-08 10:52:23 +0200

    unnecessary files removed

--- src/data/metadata.js
+++ src/data/metadata.js
@@ -9,36 +9,5 @@ module.exports = {
       title: "Pagina di test",
       description: "Questa è una Pagina di test",
     }
-  },
-  en: {
-    url,
-    title: title,
-    description: "TODO",
-    feed: {
-      subtitle: "TODO",
-      filename: "feed.xml",
-      path: "/feed/en.feed.xml",
-      id: "TODO",
-    },
-    jsonfeed: {
-      path: "/feed/en.feed.json",
-      url: `${url}/feed/en.feed.json`,
-    },
-    source: {
-      label: 'github',
-      link: 'https://github.com/moody-person/11ty-starter'
-    },
-    author: {
-      url,
-      name: "Your name",
-      email: "example@mail.com",
-    },
-    posts: {
-      title: `Posts | ${title}`,
-      description: "Blog posts list",
-    },
-    about: {
-      title: `About | ${title}`,
-    },
-  },
+  }
 };
